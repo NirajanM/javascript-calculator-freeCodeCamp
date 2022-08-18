2c25d593de7d9f76cd82088c7a8d4ae148985dc8
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-18 12:12:18 +0545

    finally tauko futney awastaa ma gayera milayee - operator ko logic, took few hours damn!

--- index.js
+++ index.js
@@ -71,88 +71,120 @@ const buttons = [
 function App() {
     const [output, setOutput] = React.useState("0");
     const [lastInput, setLastInput] = React.useState("");
+    const [secondLastInput, setSecondLastInput] = React.useState("");
     function handleClick(clickedButton) {
-        if ((lastInput === "+") || (lastInput === "-") || (lastInput === "/") || (lastInput === "*")) {
-            if ((clickedButton === "+") || (clickedButton === "-") || (clickedButton === "/") || (clickedButton === "*")) {
-                let newOutput = output.slice(0, output.length);
-                console.log(newOutput);
+        let outputTemp = output;
+        let length = outputTemp.length;
+        setSecondLastInput(output[length - 1]);
+        if ((clickedButton === "*") || (clickedButton === "/") || (clickedButton === "+")) {
+            if ((lastInput === "+") || (lastInput === "/") || (lastInput === "*") || (lastInput === "-")) {
+                outputTemp = outputTemp.slice(0, length - 1);
             }
+        }
+        if ((clickedButton === "-")) {
+            if ((lastInput === "+") || (lastInput === "-")) {
+                outputTemp = outputTemp.slice(0, length - 1);
+            }
+        }
+        if ((secondLastInput === "*") || (secondLastInput === "/")) {
+            if ((clickedButton === "*") || (clickedButton === "/") || (clickedButton === "+")) {
+                outputTemp = outputTemp.slice(0, length - 2);
+            }
+        }
+
+        if (outputTemp == "0") {
+            outputTemp = parseInt(outputTemp);
         }
         switch (clickedButton) {
             case "AC":
                 setOutput(0);
                 setLastInput("");
                 break;
+
             case "1":
-                const concatOne = (parseInt(output) === 0 ? "" : output) + "1";
+                const concatOne = (outputTemp === 0 ? "" : outputTemp) + "1";
                 setOutput(concatOne);
                 setLastInput("1");
                 break;
+
             case "2":
-                const concatTwo = (parseInt(output) === 0 ? "" : output) + "2";
+                const concatTwo = (outputTemp === 0 ? "" : outputTemp) + "2";
                 setOutput(concatTwo);
                 setLastInput("2");
                 break;
+
             case "3":
-                const concatThree = (parseInt(output) === 0 ? "" : output) + "3";
+                const concatThree = (outputTemp === 0 ? "" : outputTemp) + "3";
                 setOutput(concatThree);
                 setLastInput("3");
                 break;
+
             case "4":
-                const concatFour = (parseInt(output) === 0 ? "" : output) + "4";
+                const concatFour = (outputTemp === 0 ? "" : outputTemp) + "4";
                 setOutput(concatFour);
                 setLastInput("4");
                 break;
+
             case "5":
-                const concatFive = (parseInt(output) === 0 ? "" : output) + "5";
+                const concatFive = (outputTemp === 0 ? "" : outputTemp) + "5";
                 setOutput(concatFive);
                 setLastInput("5");
                 break;
+
             case "6":
-                const concatSix = (parseInt(output) === 0 ? "" : output) + "6";
+                const concatSix = (outputTemp === 0 ? "" : outputTemp) + "6";
                 setOutput(concatSix);
                 setLastInput("6");
                 break;
+
             case "7":
-                const concatSeven = (parseInt(output) === 0 ? "" : output) + "7";
+                const concatSeven = (outputTemp === 0 ? "" : outputTemp) + "7";
                 setOutput(concatSeven);
                 setLastInput("7");
                 break;
+
             case "8":
-                const concatEight = (parseInt(output) === 0 ? "" : output) + "8";
+                const concatEight = (outputTemp === 0 ? "" : outputTemp) + "8";
                 setOutput(concatEight);
                 setLastInput("8");
                 break;
+
             case "9":
-                const concatNine = (parseInt(output) === 0 ? "" : output) + "9";
+                const concatNine = (outputTemp === 0 ? "" : outputTemp) + "9";
                 setOutput(concatNine);
                 setLastInput("9");
                 break;
+
             case "0":
-                const concatZero = (parseInt(output) === 0 ? "" : output) + "0";
-                parseInt(output) === 0 ? setOutput("0") : setOutput(concatZero);
+                const concatZero = (parseInt(outputTemp) === 0 ? "" : outputTemp) + "0";
+                outputTemp === 0 ? setOutput("0") : setOutput(concatZero);
                 setLastInput("0");
                 break;
+
             case "/":
-                const divide = (parseInt(output) === 0 ? "" : output) + "/";
+                const divide = (outputTemp === 0 ? "" : outputTemp) + "/";
                 setOutput(divide);
                 setLastInput("/");
                 break;
+
             case "+":
-                const add = (parseInt(output) === 0 ? "" : output) + "+";
+                const add = (outputTemp === 0 ? "" : outputTemp) + "+";
                 setOutput(add);
                 setLastInput("+");
                 break;
+
             case "*":
-                const multiply = (parseInt(output) === 0 ? "" : output) + "*";
+                const multiply = (outputTemp === 0 ? "" : outputTemp) + "*";
                 setOutput(multiply);
                 setLastInput("*");
                 break;
+
             case "-":
-                const subtract = (parseInt(output) === 0 ? "" : output) + "-";
+                const subtract = (outputTemp === 0 ? "" : outputTemp) + "-";
                 setOutput(subtract);
                 setLastInput("-");
                 break;
+
             case ".":
                 let count = 0;
                 let countForSymbol = 0;
@@ -164,13 +196,16 @@ function App() {
                         ++countForSymbol;
                     }
                 }
-                const dot = (parseInt(output) === 0 ? "" : output) + ".";
+                const dot = (outputTemp === 0 ? "" : outputTemp) + ".";
                 (count > countForSymbol) ? null : setOutput(dot);
+                setLastInput(".");
                 break;
+
             case "=":
                 const evaluated = eval(output);
                 setOutput(evaluated);
                 break;
+
             default:
                 break;
         }
