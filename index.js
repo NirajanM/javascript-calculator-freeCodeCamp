bc90d5a196742de048bf447ac71a7c8b5ce48ed8
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-18 08:40:10 +0545

    styled all number and buttons with respective required id, it looked good

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
+const buttons = [
+    {
+        name: "AC",
+        id: "clear"
+    },
+    {
+        name: "/",
+        id: "divide"
+    },
+    {
+        name: "X",
+        id: "multiply"
+    },
+    {
+        name: "7",
+        id: "seven"
+    },
+    {
+        name: "8",
+        id: "eight"
+    },
+    {
+        name: "9",
+        id: "nine"
+    },
+    {
+        name: "-",
+        id: "subtract"
+    },
+    {
+        name: "4",
+        id: "four"
+    },
+    {
+        name: "5",
+        id: "five"
+    },
+    {
+        name: "6",
+        id: "six"
+    },
+    {
+        name: "+",
+        id: "add"
+    },
+    {
+        name: "1",
+        id: "one"
+    },
+    {
+        name: "2",
+        id: "two"
+    },
+    {
+        name: "3",
+        id: "three"
+    },
+    {
+        name: "=",
+        id: "equals"
+    },
+    {
+        name: "0",
+        id: "zero"
+    },
+    {
+        name: ".",
+        id: "decimal"
+    }
+];
 function App() {
     return (
         <>
-            <h1>hello world</h1>
+            <div id="MyCalculator">
+                <div id="buttonsHolder">
+                    {buttons.map((button) => {
+                        return (
+                            <button id={button.id}>{button.name}</button>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     );
 }
